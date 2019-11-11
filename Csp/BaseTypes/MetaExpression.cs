78cf27b445b2d79724bc86dbdb3560d3b537d5b6
Iain <lifebeyondfife@users.noreply.github.com>
2019-11-11 18:46:18 +0000

    Fix Windows file format issues

---
+++ Csp/BaseTypes/MetaExpression.cs
@@ -0,0 +1,18 @@
+﻿/*
+  Copyright © Iain McDonald 2010-2019
+  
+  This file is part of Decider.
+
+  Unlike the Expression type which is wholly supported on its own, the MetaExpression relies
+  on the values of other supporting variables. Thus, if those variables change, the bounds
+  of the MetaExpression need to be re-evaluated.
+*/
+using System.Collections.Generic;
+
+namespace Decider.Csp.BaseTypes
+{
+	public interface IMetaExpression<T>
+	{
+		IList<IVariable<T>> Support { get; }
+	}
+}
