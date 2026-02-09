b724cc15aef7c3a32e0acb17ebf6d5d09a151315
Iain McDonald <iain.m.mcdonald+github@gmail.com>
2026-02-09 19:47:57 +0000

    feat(cumulative): improve timetabling propagation

--- Csp/Global/CumulativeInteger.cs
+++ Csp/Global/CumulativeInteger.cs
@@ -92,10 +92,15 @@ public class CumulativeInteger : IConstraint
 		{
 			changed = false;
 
-			if (CheckResourceProfile())
+			var profile = BuildCompulsoryProfile();
+
+			foreach (var demand in profile.Values)
 			{
-				result = ConstraintOperationResult.Violated;
-				return;
+				if (demand > this.capacity)
+				{
+					result = ConstraintOperationResult.Violated;
+					return;
+				}
 			}
 
 			for (var i = 0; i < this.starts.Count; ++i)
@@ -107,7 +112,7 @@ public class CumulativeInteger : IConstraint
 
 				foreach (var candidateStart in this.starts[i].Domain)
 				{
-					if (IsInfeasible(i, candidateStart))
+					if (IsInfeasibleWithProfile(i, candidateStart, profile))
 						valuesToRemove.Add(candidateStart);
 				}
 
@@ -131,7 +136,7 @@ public class CumulativeInteger : IConstraint
 		}
 	}
 
-	private bool CheckResourceProfile()
+	private Dictionary<int, int> BuildCompulsoryProfile()
 	{
 		var profile = new Dictionary<int, int>();
 
@@ -153,42 +158,32 @@ public class CumulativeInteger : IConstraint
 					profile[t] = 0;
 
 				profile[t] += this.demands[i];
-
-				if (profile[t] > this.capacity)
-					return true;
 			}
 		}
 
-		return false;
+		return profile;
 	}
 
-	private bool IsInfeasible(int taskIndex, int candidateStart)
+	private bool IsInfeasibleWithProfile(int taskIndex, int candidateStart, Dictionary<int, int> profile)
 	{
 		var end = candidateStart + this.durations[taskIndex];
 		var demand = this.demands[taskIndex];
 
+		var est = this.starts[taskIndex].Domain.LowerBound;
+		var lst = this.starts[taskIndex].Domain.UpperBound;
+		var duration = this.durations[taskIndex];
+		var hasCompulsoryPart = lst < est + duration;
+
+		var compulsoryStart = hasCompulsoryPart ? lst : int.MaxValue;
+		var compulsoryEnd = hasCompulsoryPart ? est + duration : int.MinValue;
+
 		for (var t = candidateStart; t < end; ++t)
 		{
-			var totalDemand = demand;
+			if (!profile.TryGetValue(t, out var profileDemand))
+				profileDemand = 0;
 
-			for (var j = 0; j < this.starts.Count; ++j)
-			{
-				if (j == taskIndex)
-					continue;
-
-				var est = this.starts[j].Domain.LowerBound;
-				var lst = this.starts[j].Domain.UpperBound;
-				var duration = this.durations[j];
-
-				if (lst >= est + duration)
-					continue;
-
-				var compulsoryStart = lst;
-				var compulsoryEnd = est + duration;
-
-				if (t >= compulsoryStart && t < compulsoryEnd)
-					totalDemand += this.demands[j];
-			}
+			var taskInCompulsory = t >= compulsoryStart && t < compulsoryEnd;
+			var totalDemand = taskInCompulsory ? profileDemand : profileDemand + demand;
 
 			if (totalDemand > this.capacity)
 				return true;
